e82461841f8728bf2c36550c0a57b034cf7c5cfa
Gabriel Medina <gmedina.santos@gmail.com>
2014-09-04 13:40:27 -0300

    Alterando o cesar.js
    Adicionando nova função para remoção de acentuação e caracteres
    especiais.

--- cifra-cesar/js/cesar.js
+++ cifra-cesar/js/cesar.js
@@ -1,10 +1,11 @@
-/* 
+/*
 	Cifra de César
 */
 
 /* Cifrar */
 function cifrar(alfabeto, texto, valor){
 	var cifrado = '';
+	texto = remover_acento(texto);
 
 	for(var i = 0; i < texto.length; i += 1){
 		switch(texto[i]){
@@ -32,6 +33,7 @@ function cifrar(alfabeto, texto, valor){
 /* Decifrar */
 function decifrar(alfabeto, texto, valor){
 	var decifrado = '';
+	texto = remover_acento(texto);
 
 	for(var i = 0; i < texto.length; i += 1){
 		switch(texto[i]){
@@ -60,3 +62,21 @@ function decifrar(alfabeto, texto, valor){
 	return decifrado;
 }
 
+
+/* Retirar acentuação */
+function remover_acento(texto){
+	str_acento= 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ';
+	str_sem_acento = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+
+	var nova = '';
+
+	for(var i = 0; i < texto.length; i++) {
+		if(str_acento.indexOf(texto.charAt(i)) != -1) {
+			nova += str_sem_acento.substr(str_acento.search(texto.substr(i,1)),1);
+		} else {
+			nova += texto.substr(i,1);
+		}
+	}
+
+	return nova;
+}
